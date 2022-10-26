c091987553a51c908e5e7e4b2642bac6ca6b29a6
高宇 <673395239@qq.com>
2022-10-26 10:24:23 +0800

    feat:新增deploy脚本

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build:prod
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:zhihuifanqiechaodan/vue3-admin-template.git master:gh-pages
+
+cd -
